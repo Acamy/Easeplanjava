745c595a8c1369707a4c9af2b80d4a7bc90255d4
hebh <hebaohua@hust.edu.cn>
2018-01-26 21:33:45 +0800

    day5

---
+++ src/main/java/com/hebaohua/part1_advancedJava/day5/foxAndRabbit/cell/Cell.java
@@ -0,0 +1,7 @@
+package com.hebaohua.part1_advancedJava.day5.foxAndRabbit.cell;
+
+import java.awt.Graphics;
+ 
+public interface Cell {
+	void draw(Graphics g, int x, int y, int size);
+}
---
+++ src/main/java/com/hebaohua/part1_advancedJava/day5/foxAndRabbit/field/Location.java
@@ -0,0 +1,15 @@
+package com.hebaohua.part1_advancedJava.day5.foxAndRabbit.field;
+
+public class Location {
+	private int row;
+	private int col;
+	
+	public Location(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public int getRow() { return row; }
+	
+	public int getCol() { return col; }
+}
